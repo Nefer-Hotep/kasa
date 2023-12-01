cc46c66f9759e307846d7ab32cdccbda6b8c3293
Régis EFF <86823724+Nefer-Hotep@users.noreply.github.com>
2023-12-01 12:13:47 +0100

    feat: add fetch data on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,36 @@
+import { useState, useEffect } from 'react';
 import Banner from '../components/Banner';
 
 function Home() {
+  const [items, setItems] = useState([]);
+
+  const getData = () => {
+    fetch('data.json', {
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((dataJson) => setItems(dataJson));
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <>
       <main className='main'>
         <Banner />
         <section className='main__gallery'>
           <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
+            {items.map((item, index) => (
+              <li key={index}>
+                <img src={item.cover} alt='' />
+                <h2>{item.title}</h2>
+              </li>
+            ))}
           </ul>
         </section>
       </main>
